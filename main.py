76dc0989679bb744ed5fdfe8762a59d0249b0b27
Fuyuki-Kuro <fuyuki.g21@gmail.com>
2025-04-28 21:35:49 -0300

    Inicio

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from app.routes import views_new as views
+
+app = FastAPI(title="FastAPI App", version="1.0.0")
+
+# Monta arquivos estáticos (CSS, imagens etc)
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+# Inclui rotas de páginas
+app.include_router(views.router)
