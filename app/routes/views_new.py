87ad4c87f59a1542af3b9877efbd1b2e61a37b30
Fuyuki-Kuro <fuyuki.g21@gmail.com>
2025-04-30 19:24:13 -0300

    dssa

--- app/routes/views_new.py
+++ app/routes/views_new.py
@@ -196,7 +196,7 @@ async def list_services(
         "request_service": user_type == "cliente"
     }
     services = list(svc.collection.find())
-    print("Serviços teste")
+    print("Serviços teste meu ovo")
     return templates.TemplateResponse(
         "services.html",
         {"request": request, "services": services, "actions": actions, "menu": menu_active(user_type)}
