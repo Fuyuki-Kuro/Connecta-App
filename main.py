e3f488308a4879d6f81155c63d060d6959795c78
Dev Cansado <45550842+rafaellima-py@users.noreply.github.com>
2025-05-02 17:17:52 -0300

    Teste

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from app.routes import views_new as views
+from app.routes import plugins 
 
 app = FastAPI(title="FastAPI App", version="1.0.0")
 
@@ -9,5 +10,4 @@ app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 # Inclui rotas de páginas
 app.include_router(views.router)
-
-# Inclui rotas de API
+app.include_router(plugins.router)
