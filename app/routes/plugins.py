919af6df20b8e31b31418aa159a664be656c0925
Dev Cansado <45550842+rafaellima-py@users.noreply.github.com>
2025-05-02 17:20:01 -0300

    Implementação de rota plugin

--- app/routes/plugins.py
+++ app/routes/plugins.py
@@ -19,4 +19,5 @@ descricao: str,
     
     if not cookie:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token inválido")
+    return {"nome": nome, "tipo": tipo, "descricao": descricao}
 
